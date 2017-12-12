b5a8d5a720980762a3a88365cd7faba7c868510c
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-12 14:59:03 +0100

    Adds get_zero_twist() for initialisation of Twist and first steps of file reading

--- catkin_ws/src/turtlesim_expl/src/basic_mover.py
+++ catkin_ws/src/turtlesim_expl/src/basic_mover.py
@@ -7,12 +7,55 @@ from geometry_msgs.msg import Twist
 PI = 3.1415926535897
 
 def move():
-	# Start a new node
-	rospy.init_node('robot_mover', anonymous=True)
-	velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-	vel_msg = Twist()
-	
-	pass
+    """ Move robot according to movement file """
+
+    # Start a new node
+    rospy.init_node('robot_mover', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+
+    file_path = os.path.join(os.getcwd(), "move")
+    if not os.path.isfile():
+        print("No movement file found")
+        return
+
+    current_line = 0
+    file_contents = []
+
+    # Try to read the whole movement file
+    try:
+        file_reader = open(file_path)
+        file_contents = file_reader.readlines()
+        file_reader.close()
+    except:
+        print("Couldn't read movement file")
+        return
+
+    # While loop to assure that Ctrl-C can exit the app
+    while not rospy.is_shutdown():
+        vel_msg = get_zero_twist()
+
+        if current_line >= len(file_contents):
+            print("End of movement file reached")
+            return
+        
+        # Read next movement command
+        # ...
+
+    pass
+
+def get_zero_twist():
+    """ Return new twist initialised to zero """
+
+    new_twist = Twist()
+
+    new_twist.linear.x = 0
+    new_twist.linear.y = 0
+    new_twist.linear.z = 0
+    new_twist.angular.x = 0
+    new_twist.angular.y = 0
+    new_twist.angular.z = 0
+
+    return new_twist
 
 if __name__ == '__main__':
     try:

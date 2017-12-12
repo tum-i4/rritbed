de03362b3c61779d17ff68eea2fe87ac249cb862
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-12 14:05:57 +0100

    Adds first code stub basic_mover.py

---
+++ catkin_ws/src/turtlesim_expl/src/basic_mover.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+PI = 3.1415926535897
+
+def move():
+	# Start a new node
+	rospy.init_node('robot_mover', anonymous=True)
+	velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+	vel_msg = Twist()
+	
+	pass
+
+if __name__ == '__main__':
+	try:
+		move()
+	except rospy.ROSInterruptException:
+		pass

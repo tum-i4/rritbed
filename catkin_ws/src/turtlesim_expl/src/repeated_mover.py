77203599fa4c73d175b328f918fe8363ecd3f37b
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-14 12:14:39 +0100

    Adds repeated_mover for infinite moving

---
+++ catkin_ws/src/turtlesim_expl/src/repeated_mover.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+""" Repeated mover - indefinitely repeats basic mover """
+
+import basic_mover
+import rospy
+
+
+def repeat_move():
+	""" Indefinitely move robot according to movement file """
+	while not rospy.is_shutdown():
+		basic_mover.move()
+
+
+if __name__ == '__main__':
+	try:
+		repeat_move()
+	except rospy.ROSInterruptException:
+		pass

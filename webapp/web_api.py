073d63864e9e943e33b7c357cccc00dcb0211479
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-18 13:52:15 +0100

    Fixes code in Web API, outputs correct log format now

--- webapp/web_api.py
+++ webapp/web_api.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-from bottle import post, run, template, request, response
+""" Web API """
+
 import uuid
 import time
+from bottle import post, run, template, request, response
 
 @post("/log")
 def log():
@@ -17,34 +19,29 @@ def get_log_string():
 	time_unix_now = time.time()
 	time_utc_now = time.gmtime(time_unix_now)
 
-	return ("""{
-		"\"origin\" : \"some.java.class\", // service
-		\"abcd_version\" : \"5.2.3\",
-		\"hub\" : \"ABCD\",
-		\"level\" : \"DEBUG\", // Info, Alert
-		\"timestamp\" : \"2017-10-12T01:12:12.123Z\",
-		\"appID\" : \"ABCD\",
-		\"vin\" : \"{vin}\", // ##ROS##
-		\"abcd_topic\" : \"abcd_svds\",
-		\"environment\" : \"PROD\", // DEV (development), PROD (customer
-		\"timeUTC\" : \"{time_utc}\",
-		\"context\" : {
-			\"str\" : {
-			\"client\" : \"webapi\",
-			\"method\" : \"METHOD NAME SEE CLASS\"
-			}
-		},
-		\"threadID\" : \"http-bla-bla-bla\",
-		\"logMessage\" : \"getXForUser returned 0 vins\",
-		\"transactionID\" : \"{transaction_uuid}\",
-		\"logID\" : \"{log_uuid}\",
-		\"version\" : \"1\",
-		\"timeUnix\" : {time_unix}
-	}""").format(transaction_uuid=uuid.uuid4(),
-		log_uuid=uuid.uuid4(),
-		time_utc=time_utc_now,
-		time_unix=time_unix_now,
-		vin="A192738")
+	log_message = ("\"origin\" : \"some.java.class\", // service"
+		"\"abcd_version\" : \"5.2.3\","
+		"\"hub\" : \"ABCD\","
+		"\"level\" : \"DEBUG\", // Info, Alert"
+		"\"timestamp\" : \"2017-10-12T01:12:12.123Z\","
+		"\"appID\" : \"ABCD\","
+		"\"vin\" : \"" + "A192738" + "\", // ##ROS##"
+		"\"abcd_topic\" : \"abcd_svds\","
+		"\"environment\" : \"PROD\", // DEV (development), PROD (customer)"
+		"\"timeUTC\" : \"" + time.strftime("%Y-%m-%d %H:%M:%S", time_utc_now) + "\","
+		"\"context\" : {"
+		"	\"str\" : {"
+		"	\"client\" : \"webapi\","
+		"	\"method\" : \"METHOD NAME SEE CLASS\""
+		"	}"
+		"},"
+		"\"threadID\" : \"http-bla-bla-bla\","
+		"\"logMessage\" : \"getXForUser returned 0 vins\","
+		"\"transactionID\" : \"" + uuid.uuid4().__str__() + "\","
+		"\"logID\" : \"" + uuid.uuid4().__str__() + "\","
+		"\"version\" : \"1\","
+		"\"timeUnix\" : " + str(int(time_unix_now)))
+	return ("{" + log_message + "}")
 
 
 run(host="localhost", port=5000)

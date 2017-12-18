9367a179250fd169ad1c1793e9d174580f7fbfa3
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-18 16:56:22 +0100

    Updates init script

--- init_ros.sh
+++ init_ros.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-gnome-terminal -e roscore
-sleep 1
-
-gnome-terminal -e rosrun turtlesim turtlesim_node __name:=ecu1
-gnome-terminal -e rosrun turtlesim turtlesim_node __name:=ecu2
-sleep 1
-
 cd ~/thesis/ma-ros_git/catkin_ws
 catkin_make
 source devel/setup.bash
 
-cd ~/thesis/ma-ros_git/catkin_ws/src/turtlesim_expl/src
-rosrun turtlesim_expl basic_mover.py
+roslaunch ~/thesis/ma-ros_git/catkin_ws/src/turtlesim_expl/src/ros.launch
 
 cd ~/thesis/ma-ros_git

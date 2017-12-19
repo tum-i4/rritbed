69bf42f66ef06991cb994f6e91bd5fc05d06855d
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-19 16:49:13 +0100

    Makes logger object-oriented to save state of last publish

--- catkin_ws/src/turtlesim_expl/src/logger.py
+++ catkin_ws/src/turtlesim_expl/src/logger.py
@@ -14,39 +14,52 @@ DATA_2 = """{
 	"vin": "A232758"
 	}"""
 
-COLOUR_1_PATH = "/ecu1/turtle1/color_sensor"
-COLOUR_2_PATH = "/ecu2/turtle1/color_sensor"
+#COLOUR_1_PATH = "/ecu1/turtle1/color_sensor"
+#COLOUR_2_PATH = "/ecu2/turtle1/color_sensor"
 
 
-def log_colour_1(data):
-	""" Colour logging for node 1 """
-	log_colour(data, DATA_1)
+
+class Logger:
+
+	last_colour = None
+
+	def init(self):
+		""" Initialise logger """
+
+		rospy.init_node('logger', anonymous=True)
+
+		#rospy.Subscriber(COLOUR_1_PATH, Color, self.log_colour_1)
+		#rospy.Subscriber(COLOUR_2_PATH, Color, self.log_colour_2)
+
+		rospy.Subscriber("/turtle1/color_sensor", Color, self.log_colour_1)
+	
+
+	def log_colour_1(self, data):
+		""" Colour logging for node 1 """
+		self.log_colour(data, DATA_1)
 
 
-def log_colour_2(data):
-	""" Colour logging for node 2 """
-	log_colour(data, DATA_2)
+	def log_colour_2(self, data):
+		""" Colour logging for node 2 """
+		self.log_colour(data, DATA_2)
 
 
-def log_colour(log_data, request):
-	""" Colour logging """
+	def log_colour(self, log_data, request):
+		""" Colour logging """
 
-	rospy.loginfo(log_data)
+		if self.last_colour == log_data:
+			return
 
-	#requests.post(URL + "/log", data=request)
+		self.last_colour = log_data
+		rospy.loginfo(log_data)
 
+		#requests.post(URL + "/log", data=request)
 
-def logger():
-	""" Main function """
-
-	rospy.init_node('logger', anonymous=True)
-
-	rospy.Subscriber(COLOUR_1_PATH, Color, log_colour_1)
-	rospy.Subscriber(COLOUR_2_PATH, Color, log_colour_2)
-
-	# spin() keeps python from exiting until this node is stopped
-	rospy.spin()
 
 
 if __name__ == "__main__":
-	logger()
+	LOGGER = Logger()
+	LOGGER.init()
+
+	# spin() keeps python from exiting until this node is stopped
+	rospy.spin()

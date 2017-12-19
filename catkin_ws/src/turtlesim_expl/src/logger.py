d4565f54e017dfc070b0e5ff6ebf060d2cb75096
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-19 16:30:50 +0100

    Updates logger with two colour logging methods (WIP)

--- catkin_ws/src/turtlesim_expl/src/logger.py
+++ catkin_ws/src/turtlesim_expl/src/logger.py
@@ -3,18 +3,37 @@
 
 import requests
 import rospy
-from rosgraph_msgs.msg import Log
+from turtlesim.msg import Color
 
 URL = "http://localhost:5000"
-DATA = """{
+
+DATA_1 = """{
 	"vin": "A192738"
 	}"""
+DATA_2 = """{
+	"vin": "A232758"
+	}"""
+
+COLOUR_1_PATH = "/ecu1/turtle1/color_sensor"
+COLOUR_2_PATH = "/ecu2/turtle1/color_sensor"
 
 
-def log(data):
-	""" TODO currently logs, should call API """
+def log_colour_1(data):
+	""" Colour logging for node 1 """
+	log_colour(data, DATA_1)
 
-	requests.post(URL + "/log", data=DATA)
+
+def log_colour_2(data):
+	""" Colour logging for node 2 """
+	log_colour(data, DATA_2)
+
+
+def log_colour(log_data, request):
+	""" Colour logging """
+
+	rospy.loginfo(log_data)
+
+	#requests.post(URL + "/log", data=request)
 
 
 def logger():
@@ -22,7 +41,8 @@ def logger():
 
 	rospy.init_node('logger', anonymous=True)
 
-	rospy.Subscriber("/rosout", Log, log)
+	rospy.Subscriber(COLOUR_1_PATH, Color, log_colour_1)
+	rospy.Subscriber(COLOUR_2_PATH, Color, log_colour_2)
 
 	# spin() keeps python from exiting until this node is stopped
 	rospy.spin()

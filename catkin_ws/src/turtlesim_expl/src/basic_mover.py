b779a14f74a9bc5bce63af22c5ce720d091b0183
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-19 15:55:32 +0100

    Moves helper methods to static helper file

--- catkin_ws/src/turtlesim_expl/src/basic_mover.py
+++ catkin_ws/src/turtlesim_expl/src/basic_mover.py
@@ -6,8 +6,7 @@ import time
 import rospy
 from geometry_msgs.msg import Twist
 
-PI = 3.1415926535897
-MOVE_FILE_PATH = "/ros"
+import move_helper
 
 
 def move():
@@ -18,7 +17,7 @@ def move():
 	velocity_publisher = rospy.Publisher("turtle1/cmd_vel", Twist, queue_size=10)
 
 	# Used to use os.getcwd()
-	file_path = os.path.join(MOVE_FILE_PATH, "move")
+	file_path = os.path.join(move_helper.BASE_PATH, "move")
 	if not os.path.isfile(file_path):
 		rospy.logwarn("No movement file found in" + file_path)
 		return
@@ -39,7 +38,7 @@ def move():
 
 	# While loop to assure that Ctrl-C can exit the app
 	while not rospy.is_shutdown():
-		vel_msg = get_zero_twist()
+		vel_msg = move_helper.get_zero_twist()
 
 		if current_line >= len(file_contents):
 			rospy.loginfo("End of movement file reached")
@@ -55,7 +54,7 @@ def move():
 		next_line = next_line[:newline_index]
 
 		try:
-			vel_msg = get_twist_from_string(next_line)
+			vel_msg = move_helper.get_twist_from_string(next_line)
 		except ValueError:
 			rospy.logwarn("Invalid value read from line:\n" + next_line)
 			return
@@ -66,45 +65,7 @@ def move():
 		time.sleep(0.5)
 
     # Make sure to stop robot after the program has been cancelled
-	velocity_publisher.publish(get_zero_twist())
-
-
-def get_zero_twist():
-	""" Return new twist initialised to zero """
-
-	new_twist = Twist()
-
-	new_twist.linear.x = 0
-	new_twist.linear.y = 0
-	new_twist.linear.z = 0
-	new_twist.angular.x = 0
-	new_twist.angular.y = 0
-	new_twist.angular.z = 0
-
-	return new_twist
-
-
-def get_twist_from_string(value_string):
-	""" Return new twist initialised to the values given in the string """
-
-	value_array = value_string.split(",")
-
-	if not len(value_array) == 6:
-		raise ValueError("Incorrect number of elements in line")
-
-	# Might throw exception - intentional!
-	value_array = [int(x) for x in value_array]
-
-	new_twist = Twist()
-
-	new_twist.linear.x = value_array[0]
-	new_twist.linear.y = value_array[1]
-	new_twist.linear.z = value_array[2]
-	new_twist.angular.x = value_array[3]
-	new_twist.angular.y = value_array[4]
-	new_twist.angular.z = value_array[5]
-
-	return new_twist
+	velocity_publisher.publish(move_helper.get_zero_twist())
 
 
 if __name__ == "__main__":
---
+++ catkin_ws/src/turtlesim_expl/src/move_helper.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+""" Move helper functions """
+
+import os
+import time
+import rospy
+from geometry_msgs.msg import Twist
+
+PI = 3.1415926535897
+BASE_PATH = "/ros"
+
+
+@staticmethod
+def get_zero_twist():
+	""" Return new twist initialised to zero """
+
+	new_twist = Twist()
+
+	new_twist.linear.x = 0
+	new_twist.linear.y = 0
+	new_twist.linear.z = 0
+	new_twist.angular.x = 0
+	new_twist.angular.y = 0
+	new_twist.angular.z = 0
+
+	return new_twist
+
+
+@staticmethod
+def get_twist_from_string(value_string):
+	""" Return new twist initialised to the values given in the string """
+
+	value_array = value_string.split(",")
+
+	if not len(value_array) == 6:
+		raise ValueError("Incorrect number of elements in line")
+
+	# Might throw exception - intentional!
+	value_array = [int(x) for x in value_array]
+
+	new_twist = Twist()
+
+	new_twist.linear.x = value_array[0]
+	new_twist.linear.y = value_array[1]
+	new_twist.linear.z = value_array[2]
+	new_twist.angular.x = value_array[3]
+	new_twist.angular.y = value_array[4]
+	new_twist.angular.z = value_array[5]
+
+	return new_twist

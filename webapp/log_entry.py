7c896267bfbdab09fc3e9fff4b18c18278dd9e16
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-20 17:26:19 +0100

    Removes obsolete fields, adds id generation

--- webapp/log_entry.py
+++ webapp/log_entry.py
@@ -3,8 +3,10 @@
 
 import json
 import time
+import uuid
 
 class LogEntry(object):
+	""" Container class for log data """
 
 	log_entry = {
 		"origin" : "",          # Class name sending this log entry - some.java.method
@@ -15,13 +17,13 @@ class LogEntry(object):
 		"appID" : "",           # Name of the microservice using this
 		"vin" : "",             # Identifier of the car calling the microservice
 		"lib_topic" : "",       # Kibana topic name log was sent to
-		"env" : "",             # PROD, INT, TEST 
-		"threadID" : "",        # Server worker pool thread id serving this request
+		"env" : "",             # PROD, INT, TEST
+		#"threadID" : "",        # Server worker pool thread id serving this request
 		"log_message" : "",
 		"transactionID" : "",   # UUID of the request made to the server
 		"logID" : "",           # UUID of this log entry
 		"userIDs" : "[null]",   # List of (always one?) ints or [null] in string
-		"version" : "1",        # Internal kibana version - constant 1
+		#"version" : "1",        # Internal kibana version - constant 1
 		"timeUTC" : "",         # When the logging event has happened
 		"timeUnix" : 0,         # !Assumption!: Same time as timeUTC
 		"context" : {
@@ -31,9 +33,9 @@ class LogEntry(object):
 		}
 	}
 
-	def __init__(self, origin, log_lib_version, level="DEBUG", import_timestamp, appID, 
-		vin, lib_topic, env="TEST", threadID=1, log_message, transactionID,
-		logID, userIDs="[null]", version=1, timeUnix=None, context):
+	def __init__(self, origin, log_lib_version, import_timestamp, appID,
+		vin, lib_topic, log_message, context, transactionID=None, logID=None,
+		level="DEBUG", env="TEST", userIDs="[null]", timeUnix=None):
 		""" Ctor """
 
 		object.__init__(self)
@@ -46,32 +48,38 @@ class LogEntry(object):
 		self.log_entry.vin = vin
 		self.log_entry.lib_topic = lib_topic
 		self.log_entry.env = env
-		self.log_entry.threadID = threadID
 		self.log_entry.log_message = log_message
-		self.log_entry.transactionID = transactionID
-		self.log_entry.logID = logID
 		self.log_entry.userIDs = userIDs
-		self.log_entry.version = version
 		self.log_entry.context = context
+
+		self.log_entry.transactionID = self.set_or_generate_id(transactionID)
+		self.log_entry.logID = self.set_or_generate_id(logID)
 
 		self.set_time(timeUnix)
 
 
-	def set_time(self, timeUnix):
+	def set_or_generate_id(self, given_id):
+		""" Checks to see if the id is set, otherwise generates a new UUID """
+
+		if given_id is None:
+			return uuid.uuid4()
+
+		return given_id
+
+
+	def set_time(self, time_unix):
 		""" Sets timeUnix and timeUTC fields in this item's log data """
 
-		time_unix_now = time.time()
+		if time_unix is None:
+			time_unix = time.time()
 
-		if timeUnix is not None:
-			time_unix_now = timeUnix
-		
-		time_utc_now = time.gmtime(time_unix_now)
+		time_utc_now = time.gmtime(time_unix)
 
-		self.log_entry.timeUnix = str(int(time_unix_now))
+		self.log_entry.timeUnix = str(int(time_unix))
 		self.log_entry.timeUTC = time.strftime("%a %b %d %H:%M:%S UTC %Y", time_utc_now)
 
 
 	def get_log_string(self):
 		""" Creates a log string from this item's log data """
-		
+
 		return json.dumps(self.log_entry)

3e45e6521ca10f4db1a57527b81cf2c621c7b1ae
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-28 14:50:02 +0100

    Refactors BasicMover to BasicMoveStrategy adhering to the new strategy pattern

--- catkin_ws/src/turtlesim_expl/src/basic_mover.py
+++ catkin_ws/src/turtlesim_expl/src/basic_mover.py
@@ -1,85 +1,76 @@
 #!/usr/bin/env python
-""" Basic mover """
+""" Basic move strategy """
 
 import os
 import rospy
 
 import move_helper
-from turtle_interface import TurtleInterface
+from move_strategy import MoveStrategy
+from turtle_control import TurtleControl
 
 
-class BasicMover(object):
-	""" Basic mover based on movement file """
+class BasicMoveStrategy(MoveStrategy):
+	""" Basic move strategy based on movement file """
 
-	turtle_if = None
-	rate_limiter = None
+	_file_path = ""
+	_current_line = 0
+
 
 	def __init__(self):
 		""" Ctor """
-
-		object.__init__(self)
-
-		self.turtle_if = TurtleInterface()
-		self.rate_limiter = rospy.Rate(2)
+		MoveStrategy.__init__(self)
 
 
-	def move(self):
-		""" Move robot according to movement file """
+	def get_next(self):
+		""" Get next velocity from movement file """
 
 		# Used to use os.getcwd()
-		file_path = os.path.join(move_helper.BASE_PATH, "move")
-		if not os.path.isfile(file_path):
-			rospy.logwarn("No movement file found in" + file_path)
-			return
+		self._file_path = os.path.join(move_helper.BASE_PATH, "move")
+		if not os.path.isfile(self._file_path):
+			rospy.logwarn("No movement file found in" + self._file_path)
+			return None
 
-		rospy.loginfo("Starting basic movement with movement file " + file_path)
-
-		current_line = 0
+		self._current_line = 0
 		file_contents = []
 
 		# Try to read the whole movement file
 		try:
-			file_reader = open(file_path)
+			file_reader = open(self._file_path)
 			file_contents = file_reader.readlines()
 			file_reader.close()
 		except IOError:
 			rospy.logwarn("Couldn't read movement file")
-			return
+			return None
 
-		# While loop to assure that Ctrl-C can exit the app
-		while not rospy.is_shutdown():
-			vel_msg = move_helper.get_zero_twist()
+		vel_msg = move_helper.get_zero_twist()
 
-			if current_line >= len(file_contents):
-				rospy.loginfo("End of movement file reached")
-				return
+		if self._current_line >= len(file_contents):
+			rospy.loginfo("End of movement file reached")
+			return None
 
-			# Read next movement command
-			next_line = file_contents[current_line]
-			current_line += 1
+		# Read next movement command
+		next_line = file_contents[self._current_line]
+		self._current_line += 1
 
-			# Remove trailing new line
-			newline_index = len(next_line) - 1
-			assert(next_line[newline_index:] == "\n")
-			next_line = next_line[:newline_index]
+		# Remove trailing new line
+		newline_index = len(next_line) - 1
+		assert(next_line[newline_index:] == "\n")
+		next_line = next_line[:newline_index]
 
-			try:
-				vel_msg = move_helper.get_twist_from_string(next_line)
-			except ValueError:
-				rospy.logwarn("Invalid value read from line:\n" + next_line)
-				return
+		try:
+			vel_msg = move_helper.get_twist_from_string(next_line)
+		except ValueError:
+			rospy.logerr("Invalid value read from line:\n" + next_line)
+			return None
 
-			# We have read the velocity and can now publish it
-			self.turtle_if.publish(vel_msg)
-			self.rate_limiter.sleep()
-
-		# Make sure to stop robot after the program has been cancelled
-		self.turtle_if.stop_turtle()
+		# Return read velocity
+		return vel_msg
 
 
 if __name__ == "__main__":
 	try:
-		T_MOVER = BasicMover()
-		T_MOVER.move()
+		T_CONTROL = TurtleControl(BasicMoveStrategy, 2)
+		rospy.loginfo("Starting file-based mover")
+		T_CONTROL.run()
 	except rospy.ROSInterruptException:
 		pass

c805af9dc57b26596a579f68d8d126208b6b4e22
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-28 14:50:23 +0100

    Refactors RandomMover to RandomMoveStrategy adhering to the new strategy pattern

--- catkin_ws/src/turtlesim_expl/src/random_mover.py
+++ catkin_ws/src/turtlesim_expl/src/random_mover.py
@@ -1,60 +1,46 @@
 #!/usr/bin/env python
-""" Random mover """
+""" Random move strategy """
 
 import random
 import rospy
 
 import move_helper
-from turtle_interface import TurtleInterface
+from move_strategy import MoveStrategy
+from turtle_control import TurtleControl
 
-PI = 3.1415926535897
 
+class RandomMoveStrategy(MoveStrategy):
+	""" Random move strategy based on random.random """
 
-class RandomMover(object):
-	""" Random mover based on random.random """
-
-	turtle_if = None
-	rate_limiter = None
 
 	def __init__(self):
 		""" Ctor """
-
-		object.__init__(self)
-
-		self.turtle_if = TurtleInterface()
-		self.rate_limiter = rospy.Rate(2)
+		MoveStrategy.__init__(self)
 
 
-	def move(self):
+	def get_next(self):
 		""" Move robot randomly """
 
-		rospy.loginfo("Starting random walker")
+		vel_msg = move_helper.get_zero_twist()
 
-		# While loop to assure that Ctrl-C can exit the app
-		while not rospy.is_shutdown():
-			vel_msg = move_helper.get_zero_twist()
+		# Decide if the turtle walks or turns
+		turtle_walks = random.choice([True, False])
 
-			# Decide if the turtle walks or turns
-			turtle_walks = random.choice([True, False])
+		# Velocity should be between -10 and 10, linear x (walk) or angular z (turn)
+		veloc_value = random.choice(range(-10, 11))
 
-			# Velocity should be between -10 and 10, linear x (walk) or angular z (turn)
-			veloc_value = random.choice(range(-10, 11))
+		if turtle_walks:
+			vel_msg.linear.x = veloc_value
+		else:
+			vel_msg.angular.z = veloc_value
 
-			if turtle_walks:
-				vel_msg.linear.x = veloc_value
-			else:
-				vel_msg.angular.z = veloc_value
-
-			self.turtle_if.publish(vel_msg)
-			self.rate_limiter.sleep()
-
-		# Make sure to stop robot after the program has been cancelled
-		self.turtle_if.publish(move_helper.get_zero_twist())
+		return vel_msg
 
 
 if __name__ == "__main__":
 	try:
-		T_MOVER = RandomMover()
-		T_MOVER.move()
+		T_CONTROL = TurtleControl(RandomMoveStrategy, 2)
+		rospy.loginfo("Starting random walker")
+		T_CONTROL.run()
 	except rospy.ROSInterruptException:
 		pass

c7bbe32403196edff80532bb9ff37e18d10e0402
vauhochzett <vauhochzett@users.noreply.github.com>
2017-12-28 14:49:16 +0100

    Adds base MoveStrategy class

---
+++ catkin_ws/src/turtlesim_expl/src/move_strategy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+""" Move strategy """
+
+class MoveStrategy(object):
+	""" Move strategy base class """
+
+	def get_next(self):
+		""" Generate next velocity """
+		return None

2453dcd7e18f8df0d4c0107f2c7a2713b2dcba37
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-07 14:26:56 +0100

    LFO: Adds module docstring with first basic arguments

--- scripts/launch_file_orchestrator.py
+++ scripts/launch_file_orchestrator.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
-""" Launch file orchestrator """
+"""
+Launch file orchestrator
+
+Usage:
+</path/to/file> [OPTIONS]
+
+Possible OPTIONS:
+-m               : Create launch file with only one manually controlled turtle,
+                   a logger node and rosbag recording.
+                   Excludes all other options!
+-t="TURTLE_TYPE" : Choose turtle type; possible types: RANDOM, RANDOMPI, RANDOMPI1000
+-d=NUMBER        : Number of distribution generators; [0,100]
+-g=NUMBER        : Number of turtle-based generators; [0,10]
+"""
 
 import sys
 
 class LaunchFileOrchestrator(object):
-	a = 0
-
+	""" Creates a launch file based on the given arguments """
 
 	def __init__(self):
 		""" Ctor """

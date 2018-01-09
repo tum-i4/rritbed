4b2b232806aba2922804e4c240544317d7cefdcf
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-09 16:02:52 +0100

    ArgConstr: Sets min, max and default to float by default

--- catkin_ws/src/turtlesim_expl/src/generator/argument_constraint.py
+++ catkin_ws/src/turtlesim_expl/src/generator/argument_constraint.py
@@ -7,9 +7,9 @@ from sys import maxint as MAXINT
 class ArgumentConstraint(object):
 	""" Define constraints for an argument (min, max) """
 
-	min_value = 0
-	max_value = 0
 	default_value = None
+	min_value = 0.0
+	max_value = 0.0
 
 	def __init__(self, default_value=None, min_value=-MAXINT-1, max_value=MAXINT):
 		""" Ctor """
@@ -19,7 +19,8 @@ class ArgumentConstraint(object):
 		if min_value > max_value:
 			raise ValueError("min <= max")
 
-		self.min_value = min_value or self.min_value
-		self.max_value = max_value or self.max_value
+		if default_value is not None:
+			self.default_value = default_value * 1.0
 
-		self.default_value = default_value
+		self.min_value = min_value * 1.0
+		self.max_value = max_value * 1.0

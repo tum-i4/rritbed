aa7289d52f900ad16982361f8e02307718e96199
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-09 16:07:13 +0100

    ArgConstr: Makes default value required

--- catkin_ws/src/turtlesim_expl/src/generator/argument_constraint.py
+++ catkin_ws/src/turtlesim_expl/src/generator/argument_constraint.py
@@ -7,11 +7,11 @@ from sys import maxint as MAXINT
 class ArgumentConstraint(object):
 	""" Define constraints for an argument (min, max) """
 
-	default_value = None
+	default_value = 0.0
 	min_value = 0.0
 	max_value = 0.0
 
-	def __init__(self, default_value=None, min_value=-MAXINT-1, max_value=MAXINT):
+	def __init__(self, default_value, min_value=-MAXINT-1, max_value=MAXINT):
 		""" Ctor """
 
 		object.__init__(self)
@@ -19,8 +19,6 @@ class ArgumentConstraint(object):
 		if min_value > max_value:
 			raise ValueError("min <= max")
 
-		if default_value is not None:
-			self.default_value = default_value * 1.0
-
+		self.default_value = default_value * 1.0
 		self.min_value = min_value * 1.0
 		self.max_value = max_value * 1.0

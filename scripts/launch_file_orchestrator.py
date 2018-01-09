05d657d91c308e89ef0b8181448efac826e5649c
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-09 15:22:52 +0100

    LFO: Cleans up create() and updates order; adds comments and basic (commented-out) turtle logic

--- scripts/launch_file_orchestrator.py
+++ scripts/launch_file_orchestrator.py
@@ -9,6 +9,7 @@ import random
 import sys
 
 from lxml import etree as ET
+import json
 
 
 class LaunchFileOrchestrator(object):
@@ -112,17 +113,78 @@ Possible OPTIONS:
 
 
 	def create(self):
+		rand_gen = random.Random()
 		root_element = ET.Element("launch")
 
-		n1 = self._create_node_element("n", "t", "p")
-		n2 = self._create_node_element("n2", "t", "p", "ns", "-a -b")
+		# Rosbag recording
+		rosbag_folder = os.path.expanduser(os.path.join("~", "ros", "bags", "recording-all"))
+		root_element.append(
+			self._create_padded_comment("Rosbag recording to the file (prefix) {}".format(rosbag_folder)))
+		# <node pkg="rosbag" type="record" name="rosbag_recorder" args="-a -o /home/USER/ros/bags/recording-all"/>
+		root_element.append(
+			self._create_node_element("rosbag_recorder", "record", "rosbag", None, "-a -o " + rosbag_folder))
 
-		group = self._create_group([n1, n2], "NS")
+		# Logging node
+		root_element.append(
+			self._create_padded_comment("Logging"))
+		# <node ns="log" name="logger" pkg="turtlesim_expl" type="logger.py" />
+		root_element.append(
+			self._create_node_element("logger", "logger.py", "turtlesim_expl", "log"))
 
-		comment = ET.Comment("Logging")
+		# Data generation [5..100]
+		# - Based on distributions
+		# - A few parameters
+		# - Live and file based
 
-		for el in [group, n1, comment, n2]:
-			root_element.append(el)
+		selected_generators = []
+		possible_generators = []
+		number_of_generators = rand_gen.randint(5,100)
+		for i in range(0, number_of_generators):
+
+
+		# TODO: Note which generators exist as possibilities for the turtle (num to vel pipe)
+
+		# Turtle group [1]
+		# Options:
+		# - Random walk with parameter input for random seed
+		# - Data generation for walking
+		#   - Live
+		#   - File-based (fixed test cases)
+		# - Manual control
+
+		# Random mover -pi -pi1000 or float for args
+		# <node name="mover" pkg="turtlesim_expl" type="random_mover.py" args="-pi1000" />
+
+		# control_node = self._create_node_element
+
+		# if self._manual_turtle_mode:
+		# 	control_node = self._create_node_element(
+		# 		"teleop", "turtle_teleop_key", "turtlesim")
+		# 	control_node.attrib["output"] = "screen"
+
+		# root_element.append(
+		# 	self._create_turtle_group(control_node))
+
+
+#   <group ns="turtle">
+#   	<node name="turtlesim" pkg="turtlesim" type="turtlesim_node" />
+#     <!-- A random mover with seed pi1000 (31415...) -->
+# 	  <node name="mover" pkg="turtlesim_expl" type="random_mover.py" 
+#       args="-pi1000" />
+
+#     <!-- Manual turtle control -->
+#     <!-- node pkg="turtlesim" type="turtle_teleop_key" name="teleop" output="screen"/ -->
+#   </group>
+
+
+		# _manual_turtle_mode = False
+		# _identifier_file_path = ""
+		# _namespace_number = 0
+
+		# TODO: TEMP DEBUG
+		ET.dump(root_element)
+		exit()
+		# END TODO
 
 		# xml_tree = ET.ElementTree(root_element)
 
@@ -132,16 +194,6 @@ Possible OPTIONS:
 		# Based on identifier file
 		# Based on number of instances
 		# Check if identifier file length and number of instances supplied fit
-
-		root_element.append(
-			self._create_node_element("logger", "logger.py", "turtlesim_expl", "log"))
-
-		rosbag_folder = os.path.expanduser(os.path.join("~", "ros", "bags", "recording-all"))
-		root_element.append(
-			self._create_node_element("rosbag_recorder", "record", "rosbag", None, "-a -o " + rosbag_folder))
-
-		ET.dump(root_element)
-		exit()
 
 		# TODO: Write to file
 		# xml_tree.write("/path/to/file", xml_declaration=True)

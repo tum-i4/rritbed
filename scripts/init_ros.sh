d7775d62bfa3186caad85d69546fe4f3a0b44365
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-18 16:40:07 +0100

    Init script: Allows for file path to be given as argument

--- scripts/init_ros.sh
+++ scripts/init_ros.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+args=`getopt f: $*`
+
+base_path="~/thesis/ma-ros_git/scripts"
+
+if (( $# == 2 )); then
+    file_name="$2.launch"
+else
+	file_name="ros.launch"
+fi
+
+file_path="$base_path/$file_name"
+
 cd ~/thesis/ma-ros_git/catkin_ws
 catkin_make
 source devel/setup.bash
 
-roslaunch ~/thesis/ma-ros_git/scripts/ros.launch
+roslaunch $file_path
 
 cd ~/thesis/ma-ros_git/scripts

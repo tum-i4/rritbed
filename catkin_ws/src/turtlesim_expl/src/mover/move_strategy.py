0836282fcfdf1c2f0758e04df3c4bdf634f5be12
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-18 15:58:38 +0100

    Move Strat: Removes abstract method react() - implementation of move logic is chosen in class

--- catkin_ws/src/turtlesim_expl/src/mover/move_strategy.py
+++ catkin_ws/src/turtlesim_expl/src/mover/move_strategy.py
@@ -8,8 +8,3 @@ class MoveStrategy(object):
 	def get_next(self):
 		""" Generate next velocity """
 		raise NotImplementedError
-
-
-	def react(self):
-		""" Generate next velocity by reacting to the messages published by the turtle """
-		raise NotImplementedError

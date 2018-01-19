f1147a86a06735757d8de5c9c1087bb683323ef0
vauhochzett <vauhochzett@users.noreply.github.com>
2018-01-19 17:25:03 +0100

    Init script: Fixes missing user expansion

--- scripts/init_ros.sh
+++ scripts/init_ros.sh
@@ -2,20 +2,16 @@
 
 args=`getopt f: $*`
 
-base_path="~/thesis/ma-ros_git/scripts"
-
 if (( $# == 1 )); then
     file_name=$1
 else
 	file_name="ros.launch"
 fi
 
-file_path="$base_path/$file_name"
-
 cd ~/thesis/ma-ros_git/catkin_ws
 catkin_make
 source devel/setup.bash
 
-roslaunch $file_path
+roslaunch ~/thesis/ma-ros_git/scripts/$file_name
 
 cd ~/thesis/ma-ros_git/scripts

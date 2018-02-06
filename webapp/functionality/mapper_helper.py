4e547f586336cfb465e5a468ce8d5ea1db03b6c3
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-06 22:04:33 +0100

    Map Help: Refactors to class MapperBase

--- webapp/functionality/mapper_helper.py
+++ webapp/functionality/mapper_helper.py
@@ -5,47 +5,63 @@ import collections
 import math
 
 
-_X = "X"
-_Y = "Y"
+class MapperBase(object):
+	""" Base class for mappers """
 
-_SUB_SPACE_SIZE = {
-	_X: 5,
-	_Y: 5
-}
+	_X = "X"
+	_Y = "Y"
 
-
-def map_coordinate(original_coordinate, dimension):
-	""" Maps the original coordinate to our space """
-	converted_coordinate = math.floor(original_coordinate / 500)
-
-	if _dimension_invalid(converted_coordinate, dimension):
-		raise ArithmeticError("Resulting coordinate is invalid - "+
-			"was the given value from an invalid space?\n" +
-			"Given value: {}".format(original_coordinate))
-
-	return converted_coordinate
+	_SUB_SPACE_SIZE = {
+		_X: 5,
+		_Y: 5
+	}
 
 
-def assert_size(matrix):
-	""" Verifies the given matrix' size """
+	def map(self, matrix, crd_x, crd_y):
+		""" Maps given coordinates to the given matrix """
 
-	# pylint: disable-msg=C1801; (Do not use len in conditions - more understandable this way)
-	if ((not isinstance(matrix, collections.Sequence))
-		or len(matrix) == 0
-		or (not isinstance(matrix[0], collections.Sequence))
-		or len(matrix[0]) == 0):
-		raise AssertionError("Given object is either no matrix or is zero in one or more dimensions")
+		# Throws if incorrect size
+		self.assert_size(matrix)
 
-	# We definitely have a two-dimensional matrix at this point
+		# Both throw if invalid value
+		mapped_x = self.map_coordinate(crd_x, self._X)
+		mapped_y = self.map_coordinate(crd_y, self._Y)
 
-	if _len_invalid(matrix, _X) or _len_invalid(matrix[0], _Y):
-		raise AssertionError("Given matrix has incorrect dimensions.\nx: {}\ny: {}".format(
-			len(matrix), len(matrix[0])))
+		return matrix[mapped_x][mapped_y]
 
 
-def _dimension_invalid(value, dimension):
-	return value < 0 or value >= _SUB_SPACE_SIZE[dimension]
+	def map_coordinate(self, original_coordinate, dimension):
+		""" Maps the original coordinate to our space """
+		converted_coordinate = math.floor(original_coordinate / 500)
+
+		if self._dimension_invalid(converted_coordinate, dimension):
+			raise ArithmeticError("Resulting coordinate is invalid - "+
+				"was the given value from an invalid space?\n" +
+				"Given value: {}".format(original_coordinate))
+
+		return converted_coordinate
 
 
-def _len_invalid(seq, dimension):
-	return len(seq) != _SUB_SPACE_SIZE[dimension]
+	def assert_size(self, matrix):
+		""" Verifies the given matrix' size """
+
+		# pylint: disable-msg=C1801; (Do not use len in conditions - more understandable this way)
+		if ((not isinstance(matrix, collections.Sequence))
+			or len(matrix) == 0
+			or (not isinstance(matrix[0], collections.Sequence))
+			or len(matrix[0]) == 0):
+			raise AssertionError("Given object is either no matrix or is zero in one or more dimensions")
+
+		# We definitely have a two-dimensional matrix at this point
+
+		if self._len_invalid(matrix, self._X) or self._len_invalid(matrix[0], self._Y):
+			raise AssertionError("Given matrix has incorrect dimensions.\nx: {}\ny: {}".format(
+				len(matrix), len(matrix[0])))
+
+
+	def _dimension_invalid(self, value, dimension):
+		return value < 0 or value >= self._SUB_SPACE_SIZE[dimension]
+
+
+	def _len_invalid(self, seq, dimension):
+		return len(seq) != self._SUB_SPACE_SIZE[dimension]

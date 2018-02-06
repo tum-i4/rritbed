938ced409364764129bcc36cd09d4e30560940bb
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-06 22:08:46 +0100

    Map Base: Makes class members static

--- webapp/functionality/mapper_base.py
+++ webapp/functionality/mapper_base.py
@@ -17,24 +17,26 @@ class MapperBase(object):
 	}
 
 
-	def map(self, matrix, crd_x, crd_y):
+	@staticmethod
+	def map(matrix, crd_x, crd_y):
 		""" Maps given coordinates to the given matrix """
 
 		# Throws if incorrect size
-		self.assert_size(matrix)
+		MapperBase.assert_size(matrix)
 
 		# Both throw if invalid value
-		mapped_x = self.map_coordinate(crd_x, self._X)
-		mapped_y = self.map_coordinate(crd_y, self._Y)
+		mapped_x = MapperBase.map_coordinate(crd_x, MapperBase._X)
+		mapped_y = MapperBase.map_coordinate(crd_y, MapperBase._Y)
 
 		return matrix[mapped_x][mapped_y]
 
 
-	def map_coordinate(self, original_coordinate, dimension):
+	@staticmethod
+	def map_coordinate(original_coordinate, dimension):
 		""" Maps the original coordinate to our space """
 		converted_coordinate = math.floor(original_coordinate / 500)
 
-		if self._dimension_invalid(converted_coordinate, dimension):
+		if MapperBase._dimension_invalid(converted_coordinate, dimension):
 			raise ArithmeticError("Resulting coordinate is invalid - "+
 				"was the given value from an invalid space?\n" +
 				"Given value: {}".format(original_coordinate))
@@ -42,7 +44,8 @@ class MapperBase(object):
 		return converted_coordinate
 
 
-	def assert_size(self, matrix):
+	@staticmethod
+	def assert_size(matrix):
 		""" Verifies the given matrix' size """
 
 		# pylint: disable-msg=C1801; (Do not use len in conditions - more understandable this way)
@@ -54,14 +57,16 @@ class MapperBase(object):
 
 		# We definitely have a two-dimensional matrix at this point
 
-		if self._len_invalid(matrix, self._X) or self._len_invalid(matrix[0], self._Y):
+		if MapperBase._len_invalid(matrix, MapperBase._X) or MapperBase._len_invalid(matrix[0], MapperBase._Y):
 			raise AssertionError("Given matrix has incorrect dimensions.\nx: {}\ny: {}".format(
 				len(matrix), len(matrix[0])))
 
 
-	def _dimension_invalid(self, value, dimension):
-		return value < 0 or value >= self._SUB_SPACE_SIZE[dimension]
+	@staticmethod
+	def _dimension_invalid(value, dimension):
+		return value < 0 or value >= MapperBase._SUB_SPACE_SIZE[dimension]
 
 
-	def _len_invalid(self, seq, dimension):
-		return len(seq) != self._SUB_SPACE_SIZE[dimension]
+	@staticmethod
+	def _len_invalid(seq, dimension):
+		return len(seq) != MapperBase._SUB_SPACE_SIZE[dimension]

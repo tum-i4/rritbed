c71d6e0720da4cdebcdf8dbe920e7fde4d7afebf
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-06 21:31:54 +0100

    Map Help: Adds assert_size for matrizes

--- webapp/functionality/mapper_helper.py
+++ webapp/functionality/mapper_helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """ Mapper helper functions """
 
+import collections
 import math
 
 
@@ -14,3 +15,18 @@ def map_coordinate(original_coordinate):
 			"Given value: {}".format(original_coordinate))
 
 	return converted_coordinate
+
+
+def assert_size(matrix):
+	""" Verifies the given matrix' size """
+
+	# pylint: disable-msg=C1801; (Do not use len in conditions - more understandable this way)
+	if ((not isinstance(matrix, collections.Sequence))
+		or len(matrix) == 0
+		or (not isinstance(matrix[0], collections.Sequence))
+		or len(matrix[0]) == 0):
+		raise AssertionError("Given object is either no matrix or is zero in one or more dimensions")
+
+	if len(matrix) != 5 or len(matrix[0]) != 5:
+		raise AssertionError("Given matrix has incorrect dimensions.\nx: {}\ny: {}".format(
+			len(matrix), len(matrix[0])))

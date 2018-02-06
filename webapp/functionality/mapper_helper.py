0605eb1bb9c9e08ca291f436a501c6a5f112cd54
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-06 22:00:40 +0100

    Map Help: Makes sub space verification differentiate dimensions

--- webapp/functionality/mapper_helper.py
+++ webapp/functionality/mapper_helper.py
@@ -5,14 +5,20 @@ import collections
 import math
 
 
-_SUB_SPACE_SIZE = 5
+_X = "X"
+_Y = "Y"
+
+_SUB_SPACE_SIZE = {
+	_X: 5,
+	_Y: 5
+}
 
 
-def map_coordinate(original_coordinate):
+def map_coordinate(original_coordinate, dimension):
 	""" Maps the original coordinate to our space """
 	converted_coordinate = math.floor(original_coordinate / 500)
 
-	if _dimension_invalid(converted_coordinate):
+	if _dimension_invalid(converted_coordinate, dimension):
 		raise ArithmeticError("Resulting coordinate is invalid - "+
 			"was the given value from an invalid space?\n" +
 			"Given value: {}".format(original_coordinate))
@@ -32,14 +38,14 @@ def assert_size(matrix):
 
 	# We definitely have a two-dimensional matrix at this point
 
-	if _len_invalid(matrix) or _len_invalid(matrix[0]):
+	if _len_invalid(matrix, _X) or _len_invalid(matrix[0], _Y):
 		raise AssertionError("Given matrix has incorrect dimensions.\nx: {}\ny: {}".format(
 			len(matrix), len(matrix[0])))
 
 
-def _dimension_invalid(value):
-	return value < 0 or value >= _SUB_SPACE_SIZE
+def _dimension_invalid(value, dimension):
+	return value < 0 or value >= _SUB_SPACE_SIZE[dimension]
 
 
-def _len_invalid(seq):
-	return len(seq) != _SUB_SPACE_SIZE
+def _len_invalid(seq, dimension):
+	return len(seq) != _SUB_SPACE_SIZE[dimension]

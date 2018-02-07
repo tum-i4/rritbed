56a052abec0d2fd985b89a084153ddbcd803a8e6
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-08 00:53:23 +0100

    TR Tests: Adds test path

--- scripts/test_runner_tests.py
+++ scripts/test_runner_tests.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 """ Unit tests for the test runner """
 
-import random
+# pylint: disable-msg=W0212; (Illegal access to private members)
+
+import os
 import unittest
 
 from test_runner import TestRunner
@@ -9,7 +11,8 @@ from test_runner import TestRunner
 
 class Tests(unittest.TestCase):
 	""" Tests for the TestRunner """
-	pass
+
+	test_path = os.path.expanduser("~/test/test_runner_test")
 
 
 if __name__ == "__main__":

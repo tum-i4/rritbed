b81d0fee1e15f05a7405795ef990720b371955ac
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-07 01:36:14 +0100

    Tsp Map: Adds coordinate matrix generation method

--- webapp/functionality/tsp_routing_mapper.py
+++ webapp/functionality/tsp_routing_mapper.py
@@ -10,3 +10,22 @@ class TspRoutingMapper(MapperBase):
 	def map(crd_x, crd_y, targ_x, targ_y):
 		""" Maps the given position and target to some TSP route """
 		pass
+
+
+	@staticmethod
+	def _get_coordinate_matrix():
+		coordinates = [
+			[],
+			[],
+			[],
+			[],
+			[]
+		]
+
+		for i in range(0, MapperBase._get_width()):
+			for j in range(0, MapperBase._get_height()):
+				coordinates[i][j] = "{},{}".format(i, j)
+
+		MapperBase._assert_matrix_validity(coordinates)
+
+		return coordinates

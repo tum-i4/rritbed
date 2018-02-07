2f3877a119d705f778c9f26de0be289747a669b2
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-08 00:23:03 +0100

    Tests: Adds bootstrapped test class for TestRunner

---
+++ scripts/test_runner_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+""" Unit tests for the test runner """
+
+import random
+import unittest
+
+
+class Tests(unittest.TestCase):
+	pass
+
+
+if __name__ == "__main__":
+	raise NotImplementedError("Class was built to be run by a TestRunner")

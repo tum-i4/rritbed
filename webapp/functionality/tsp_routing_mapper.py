37436a7510dd0e9206569c78478011d9d54b2b4f
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-07 01:35:38 +0100

    Adds new TSP Routing Mapper

---
+++ webapp/functionality/tsp_routing_mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+""" TSP routing mapper """
+
+from functionality.mapper_base import MapperBase
+
+class TspRoutingMapper(MapperBase):
+	""" Mapping position and target to some TSP route """
+
+	@staticmethod
+	def map(crd_x, crd_y, targ_x, targ_y):
+		""" Maps the given position and target to some TSP route """
+		pass

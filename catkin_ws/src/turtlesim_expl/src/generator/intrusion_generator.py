4d6f599ba11faf66ae1980a6637116552a967311
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-09 20:22:48 +0100

    Intr Gen: Adds class docstring and ctor to class; adds intrusion identifiers as class constants

--- catkin_ws/src/turtlesim_expl/src/generator/intrusion_generator.py
+++ catkin_ws/src/turtlesim_expl/src/generator/intrusion_generator.py
@@ -4,4 +4,16 @@
 from distribution_generator import DistributionGenerator
 
 class IntrusionGenerator(DistributionGenerator):
-	pass
+	""" A wrapper for a DistributionGenerator that allows for intrusions """
+
+	ONLY_ZEROES = "zeroes"
+	HUGE_ERROR = "huge-error"
+
+	test = 0
+
+	def __init__(self, method, name, args_constraints=None, rate_in_hz=10, queue_size=10):
+		""" Ctor """
+
+		DistributionGenerator.__init__(self, method, name, args_constraints, rate_in_hz, queue_size)
+
+		raise NotImplementedError()

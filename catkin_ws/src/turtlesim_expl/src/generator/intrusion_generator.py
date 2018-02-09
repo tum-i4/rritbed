ddb737f388b9d17ea8ab5a336eab57ef6839bf0d
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-09 20:30:10 +0100

    Intr Gen: Adds basic strategy pattern for setting generate() method

--- catkin_ws/src/turtlesim_expl/src/generator/intrusion_generator.py
+++ catkin_ws/src/turtlesim_expl/src/generator/intrusion_generator.py
@@ -9,11 +9,27 @@ class IntrusionGenerator(DistributionGenerator):
 	ONLY_ZEROES = "zeroes"
 	HUGE_ERROR = "huge-error"
 
-	test = 0
+	_intrusion_generators = {
+		ONLY_ZEROES: None,
+		HUGE_ERROR: None
+	}
 
-	def __init__(self, method, name, args_constraints=None, rate_in_hz=10, queue_size=10):
+	_generator = None
+
+	def __init__(self, method, name, intrusion_mode,
+		args_constraints=None, rate_in_hz=10, queue_size=10):
 		""" Ctor """
 
 		DistributionGenerator.__init__(self, method, name, args_constraints, rate_in_hz, queue_size)
 
-		raise NotImplementedError()
+		try:
+			self.generate = self._intrusion_generators[intrusion_mode]
+		except KeyError:
+			raise NotImplementedError("Intrusion mode not implemented")
+
+
+	# pylint: disable-msg=E0202; (Attribute hides this method - intentional)
+	def generate(self, values=None):
+		""" Generate a new value based on the intrusion-wrapped distribution method """
+		# Is replaced by the respective intrusion generator
+		pass

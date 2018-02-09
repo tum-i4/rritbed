ca06077c1fabdc2ad3d39e70ab8683008faa8392
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-09 20:14:48 +0100

    Adds new IntrusionGenerator class

---
+++ catkin_ws/src/turtlesim_expl/src/generator/intrusion_generator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+""" All generator definitions and corresponding methods """
+
+from distribution_generator import DistributionGenerator
+
+class IntrusionGenerator(DistributionGenerator):
+	pass

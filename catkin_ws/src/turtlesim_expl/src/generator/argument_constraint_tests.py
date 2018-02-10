b906956df370e2ca208b8bcc32c198815eb98e94
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-10 01:08:14 +0100

    Tests: Adds empty test module for ArgumentConstraint

---
+++ catkin_ws/src/turtlesim_expl/src/generator/argument_constraint_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+""" Unit and system tests for LFO and related classes """
+
+import unittest
+
+from parameterized import parameterized
+
+from argument_constraint import ArgumentConstraint
+
+
+class Tests(unittest.TestCase):
+	""" Tests for the generator module """
+
+	pass

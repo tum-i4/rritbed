0dbd37c52aaf1d9a44c4320516f1c52d37cbe72b
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-10 01:08:40 +0100

    AC Tests: Adds test for fits() with valid inputs

--- catkin_ws/src/turtlesim_expl/src/generator/argument_constraint_tests.py
+++ catkin_ws/src/turtlesim_expl/src/generator/argument_constraint_tests.py
@@ -11,4 +11,18 @@ from argument_constraint import ArgumentConstraint
 class Tests(unittest.TestCase):
 	""" Tests for the generator module """
 
-	pass
+	@parameterized([
+		(1, 1, 1, 1), # hits both ends
+		(0, -1, 1, 0), # is default value
+		(0, -1, 1, -1), # hits min
+		(0, -1, 1, 1), # hits max
+		(0, -2, 2, 1) # random value
+	])
+	def test_fits_does(self, default_value, min_value, max_value, test_value):
+		""" Tests valid fits """
+		self._test_fits(default_value, min_value, max_value, test_value, True)
+
+
+	def _test_fits(self, default_value, min_value, max_value, test_value, expected):
+		constraint = ArgumentConstraint(default_value, min_value, max_value)
+		self.assertEqual(constraint.fits(test_value), expected)

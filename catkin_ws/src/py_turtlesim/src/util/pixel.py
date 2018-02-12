0c56730b7ec009be8a466446f25a343a8364bff4
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-12 01:59:59 +0100

    Adds Pixel storage class

---
+++ catkin_ws/src/py_turtlesim/src/util/pixel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+""" Storage class for RGB information """
+
+class Pixel(object):
+	""" Pixel class """
+
+	# pylint: disable-msg=C0103; (Invalid class attribute name)
+	r = 0
+	g = 0
+	b = 0
+
+	def __init__(self, r=0, g=0, b=0):
+		""" Ctor """
+		object.__init__(self)
+		self.r = r
+		self.g = g
+		self.b = b

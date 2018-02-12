f376017ec674109bb8316805998e22a7117e577a
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-13 00:21:13 +0100

    Point: Ensures the point properties to be int; adds update() method for setting

--- catkin_ws/src/py_turtlesim/src/util/point.py
+++ catkin_ws/src/py_turtlesim/src/util/point.py
@@ -11,5 +11,12 @@ class Point(object):
 	def __init__(self, x=0, y=0):
 		""" Ctor """
 		object.__init__(self)
-		self.x = x
-		self.y = y
+		self.x = int(x)
+		self.y = int(y)
+
+
+	def update(self, x=None, y=None):
+		""" Set the properties to the given values. """
+
+		self.x = int(x) if x is not None else self.x
+		self.y = int(y) if y is not None else self.y

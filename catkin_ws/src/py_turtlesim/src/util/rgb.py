751bb106227e3bb05a2f102279cef8744e493679
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-12 23:08:44 +0100

    Rgb: Adds our current colours as creators and MIN and MAX constants

--- catkin_ws/src/py_turtlesim/src/util/rgb.py
+++ catkin_ws/src/py_turtlesim/src/util/rgb.py
@@ -4,6 +4,9 @@
 class Rgb(object):
 	""" RGB class """
 
+	MIN = 0
+	MAX = 255
+
 	# pylint: disable-msg=C0103; (Invalid class attribute name)
 	r = 0
 	g = 0
@@ -15,3 +18,28 @@ class Rgb(object):
 		self.r = r
 		self.g = g
 		self.b = b
+
+
+	def pastel_purple(self):
+		""" Return a pastel purple colour """
+		return Rgb(r=150, g=125, b=210)
+
+
+	def pastel_yellow(self):
+		""" Return a pastel yellow colour """
+		return Rgb(r=255, g=240, b=120)
+
+
+	def pastel_green(self):
+		""" Return a pastel green colour """
+		return Rgb(r=100, g=180, b=100)
+
+
+	def pastel_blue(self):
+		""" Return a pastel blue colour """
+		return Rgb(r=100, g=180, b=250)
+
+
+	def red(self):
+		""" Return a red colour """
+		return Rgb(r=Rgb.MAX)

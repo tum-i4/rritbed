513609d0b7f2f6a47eae29925de6fc5398fdf2ef
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-13 15:35:18 +0100

    py_turtlesim.py: Ports implementation from turtlesim.cpp

--- catkin_ws/src/py_turtlesim/src/py_turtlesim.py
+++ catkin_ws/src/py_turtlesim/src/py_turtlesim.py
@@ -3,6 +3,7 @@
 Rebuilding turtlesim.cpp in Python
 """
 
+import sys
 import rospy
 from turtle_frame import TurtleFrame
 
@@ -15,4 +16,20 @@ class Turtlesim(object):
 
 		object.__init__(self)
 
-		pass
+		rospy.init_node("turtlesim", argv=sys.argv)
+
+
+	def execute(self):
+		""" Run the simulation. """
+
+		# pylint: disable-msg=W0612; (Unused variable - need to hold reference)
+		frame = TurtleFrame()
+
+		# Block until shut down
+		rospy.spin()
+
+
+
+if __name__ == "__main__":
+	SIM = Turtlesim()
+	SIM.execute()

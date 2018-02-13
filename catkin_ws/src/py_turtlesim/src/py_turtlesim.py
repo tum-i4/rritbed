d56ba73349bad83b0b907791fe77eeefd9d104c7
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-13 15:32:39 +0100

    Adds final component, the turtlesim.cpp replacement py_turtlesim.py, to project

---
+++ catkin_ws/src/py_turtlesim/src/py_turtlesim.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Rebuilding turtlesim.cpp in Python
+"""
+
+import rospy
+from turtle_frame import TurtleFrame
+
+
+class Turtlesim(object):
+	""" The turtlesim """
+
+	def __init__(self):
+		""" Ctor """
+
+		object.__init__(self)
+
+		pass

9f5d89933b53bb630c292bc00483db1c94ab2994
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-28 17:36:34 +0100

    Intrusion Definition: Adds private fields, ctor with level verification and intrusion_levels property

--- scripts/lfo_components/intrusion_definition.py
+++ scripts/lfo_components/intrusion_definition.py
@@ -5,4 +5,40 @@
 class IntrusionDefinition(object):
 	""" Container for an intrusion definition (corresponding to one launch file). """
 
-	pass
+	_intrusion_percentage = 0
+	_intrusion_level = 0
+	_intrude_turtle = True
+	_intrude_generators = True
+	_duplicate_vins = False
+
+	_levels = ["easy", "med", "hard"]
+
+
+	def __init__(self, intrusion_percentage, intrusion_level,
+		intrude_turtle=True, intrude_generators=True, duplicate_vins=False):
+		""" Ctor """
+
+		object.__init__(self)
+
+		self._intrusion_percentage = int(intrusion_percentage)
+		self._intrusion_level = self._get_level_id(intrusion_level)
+		self._intrude_turtle = intrude_turtle
+		self._intrude_generators = intrude_generators
+		self._duplicate_vins = duplicate_vins
+
+
+	@property
+	def intrusion_levels(self):
+		""" Get all possible intrusion level strings. """
+		return self._levels
+
+
+	def _get_level_id(self, intrusion_level_string):
+		""" Convert the given level string to a level id. """
+
+		assert(len(self.intrusion_levels) == 3)
+
+		if intrusion_level_string not in self.intrusion_levels:
+			raise ValueError("Invalid intrusion level string")
+
+		return self.intrusion_levels.index(intrusion_level_string)

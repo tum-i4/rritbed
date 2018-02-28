0bdc7d755c04184ea73f4d4c2e441cfab3748aa8
vauhochzett <vauhochzett@users.noreply.github.com>
2018-02-28 17:23:13 +0100

    Adds new (empty) IntrusionDefinition class

---
+++ scripts/lfo_components/intrusion_definition.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+""" Contains the IntrusionDefinition class """
+
+
+class IntrusionDefinition(object):
+	""" Container for an intrusion definition (corresponding to one launch file). """
+
+	pass

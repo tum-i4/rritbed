f841593bbddecbe46e33fe4422f247c23bfc773c
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-01 00:55:10 +0100

    Pose Pipe: Adds get_possible_processors()

--- catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
@@ -3,4 +3,11 @@
 
 class PosePipe(object):
 	""" Subscribe to a pose topic and process it with the specified PoseProcessor. """
-	pass
+
+	_possible_processors = ["country code", "poi", "tsp"]
+
+
+	@staticmethod
+	def get_possible_processors():
+		""" Get a list of possible processor names. """
+		return PosePipe._possible_processors

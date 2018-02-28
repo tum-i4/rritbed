ccdae25a1884fcf152a3ec40b012fd073b53a626
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-01 00:48:35 +0100

    Adds PosePipe and PoseProcessor classes for outsources pose handling

---
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+""" Module for the PosePipe class """
+
+class PosePipe(object):
+	""" Subscribe to a pose topic and process it with the specified PoseProcessor. """
+	pass
---
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_processor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+""" Module for the PoseProcessor class """
+
+class PoseProcessor(object):
+	""" Process poses and convert them to request objects. """
+	pass

892d8b2270b22df254a7f6ad560bfe07f75a232a
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-01 01:38:19 +0100

    Pos Proc: Removes coordinates from process* methods

--- catkin_ws/src/turtlesim_expl/src/pipes/pose_processor.py
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_processor.py
@@ -26,18 +26,18 @@ class PoseProcessor(object):
 
 	# This method is replaced by implementation
 	# pylint: disable-msg=R0201,W0613,E0202; (Could be function, unused arguments, method is hidden)
-	def process(self, crd_x, crd_y, request):
+	def process(self, request):
 		""" Process the given coordinates and enrich the request with result. """
 		raise ValueError("This is not supposed to be called.")
 
 
-	def process_cc(self, crd_x, crd_y, request):
+	def process_cc(self, request):
+		self.send_request("country-code", request, path="get")
+
+
+	def process_poi(self, request):
 		pass
 
 
-	def process_poi(self, crd_x, crd_y, request):
-		pass
-
-
-	def process_tsp(self, crd_x, crd_y, request):
+	def process_tsp(self, request):
 		pass

cfa6d2d0e846de91485d2b251155f1e7f9c0f331
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-01 01:12:34 +0100

    Pos Pip: Now stores processors in dict

--- catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
@@ -4,10 +4,17 @@
 class PosePipe(object):
 	""" Subscribe to a pose topic and process it with the specified PoseProcessor. """
 
-	_possible_processors = ["country code", "poi", "tsp"]
+	_cc_str = "country code"
+	_poi_str = "poi"
+	_tsp_str = "tsp"
+	_possible_processors = {
+		_cc_str : None,
+		_poi_str : None,
+		_tsp_str : None
+	}
 
 
 	@staticmethod
 	def get_possible_processors():
 		""" Get a list of possible processor names. """
-		return PosePipe._possible_processors
+		return PosePipe._possible_processors.keys()

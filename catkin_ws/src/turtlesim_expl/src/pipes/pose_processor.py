62fa78ce889afa828d741921d8c47fb3908f69b3
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-01 01:20:42 +0100

    Pos Proc: Adds stubs for the process methods and changes to method hiding

--- catkin_ws/src/turtlesim_expl/src/pipes/pose_processor.py
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_processor.py
@@ -5,17 +5,30 @@ class PoseProcessor(object):
 	""" Process poses and convert them to request objects. """
 
 	_name = ""
-	_processor = None
 
-	def __init__(self, name, processor):
+	def __init__(self, name, process):
 		""" Ctor """
 
 		object.__init__(self)
 
 		self._name = name
-		self._processor = processor
+		self.process = process
 
 
+	# This method is replaced by implementation
+	# pylint: disable-msg=R0201,W0613,E0202; (Could be function, unused arguments, method is hidden)
 	def process(self, crd_x, crd_y, request):
 		""" Process the given coordinates and enrich the request with result. """
+		raise ValueError("This is not supposed to be called.")
+
+
+	def process_cc(self, crd_x, crd_y, request):
+		pass
+
+
+	def process_poi(self, crd_x, crd_y, request):
+		pass
+
+
+	def process_tsp(self, crd_x, crd_y, request):
 		pass

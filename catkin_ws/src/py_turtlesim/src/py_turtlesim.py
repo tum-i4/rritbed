b4b798d57f9d71cebe11b597a2ecb288be4c6cd6
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-02 22:08:02 +0100

    py_turtlesim.py: Adds --intrusion flag and hands it to turtle frame

--- catkin_ws/src/py_turtlesim/src/py_turtlesim.py
+++ catkin_ws/src/py_turtlesim/src/py_turtlesim.py
@@ -24,11 +24,11 @@ class Turtlesim(object):
 
 
 	# pylint: disable-msg=R0201; (Method could be a function - only callable after __init__)
-	def execute(self, draw_gui=False):
+	def execute(self, draw_gui=False, intrusion=None):
 		""" Run the simulation. """
 
 		# pylint: disable-msg=W0612; (Unused variable - need to hold reference)
-		frame = TurtleFrame(draw_gui)
+		frame = TurtleFrame(draw_gui, intrusion)
 
 		# Block until shut down
 		rospy.spin()
@@ -38,9 +38,10 @@ class Turtlesim(object):
 if __name__ == "__main__":
 	PARSER = argparse.ArgumentParser(prog="tf")
 	PARSER.add_argument("--draw-gui", "-g", action="store_true", dest="draw_gui")
+	PARSER.add_argument("--intrusion", "-i", choices=["easy", "med", "hard"])
 	# Remove remapping arguments and program name
 	FILTERED_ARGV = rospy.myargv(sys.argv)[1:]
 	ARGS = PARSER.parse_args(FILTERED_ARGV)
 
 	SIM = Turtlesim()
-	SIM.execute(ARGS.draw_gui)
+	SIM.execute(ARGS.draw_gui, ARGS.intrusion)

71fa06a59a096a960504702981ced0bb369bd056
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-04 19:48:17 +0100

    Adds path verifier utility

---
+++ catkin_ws/src/turtlesim_expl/src/path_verify.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+""" verify() method """
+
+import os
+
+import generator.distribution_publisher as DP
+import mover.basic_mover as BM
+
+BASE_PATH = os.path.expanduser("~/ros")
+
+def verify():
+	""" Verify that the classes distribution_publisher.py and basic_mover.py\
+	use the correct BASE_PATH. """
+
+	result = []
+	for module in [DP, BM]:
+		msg = "{}:".format(os.path.splitext(os.path.basename(module.__file__))[0])
+		if module.BASE_PATH != BASE_PATH:
+			print("{} invalid [{}]".format(msg, module.BASE_PATH))
+			result.append(False)
+		else:
+			print("{} okay".format(msg))
+			result.append(True)
+
+	result_msg = "{}/{} tests successful".format(result.count(True), len(result))
+	print("-" * len(result_msg))
+	print(result_msg)
+
+
+
+if __name__ == "__main__":
+	verify()

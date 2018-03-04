5f244dfeaf95f4256c63b7286a0cba145223626e
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-04 18:59:09 +0100

    Point: Fixes static / instance bug

--- catkin_ws/src/py_turtlesim/src/util/point.py
+++ catkin_ws/src/py_turtlesim/src/util/point.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 """ Storage class for a point on a 2D grid """
 
+# pylint: disable-msg=C0103; (Invalid attribute / argument names x, y)
+
+
 class Point(object):
 	""" Point class """
-
-	# pylint: disable-msg=C0103; (Invalid class attribute name)
-	x = 0
-	y = 0
 
 	def __init__(self, x=0, y=0):
 		""" Ctor """

28c9a77fe369494b3226607e314b6d3ee164988b
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-04 17:29:07 +0100

    Pos Pipe: Fixes __main__

--- catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
+++ catkin_ws/src/turtlesim_expl/src/pipes/pose_pipe.py
@@ -71,7 +71,8 @@ class PosePipe(object):
 
 
 if __name__ == "__main__":
-	PP = PosePipe.create(intrusion=None, cc=50, poi=25, tsp=25)
+	PP = PosePipe.create(intrusion=None, intrusion_field="INVALID", cc=50, poi=25, tsp=25)
 	print("Possible processors: {}".format(PP.get_possible_processors()))
 	print("Processor name: {}".format(PP.get_processor_name()))
-	print("Process [x: 5] [y: 10]: {}".format(PP.process(PoseProcessor.add_to_request({}, 5, 10))))
+	print("Process [x: 5] [y: 10]: {}".format(
+		PP.process(PoseProcessor.add_to_request({}, 5, 10), label=True)))

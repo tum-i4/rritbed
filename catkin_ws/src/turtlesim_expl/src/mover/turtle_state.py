e520f9b3ed82fe2c8b1853ce1dba47344f2c9b19
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-05 14:49:43 +0100

    Adds TurtleState attr class for holding data and last_update for use in Rand Mov

---
+++ catkin_ws/src/turtlesim_expl/src/mover/turtle_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+""" TurtleState class """
+
+import time
+import attr
+
+
+@attr.s
+class TurtleState(object):
+	""" Hold a data object and last broadcast time in seconds. """
+
+	data = attr.ib()
+	last_update = attr.ib(default=0)
+
+
+	def update(self, data):
+		""" Update the data and set the last_update field to now. """
+
+		self.data = data
+		self.last_update = time.time()

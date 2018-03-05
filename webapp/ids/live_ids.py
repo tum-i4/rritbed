d2ee5328c57e3689263dd5d8c27e9bf4ff26377e
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-05 01:51:43 +0100

    Starts work on _print_and_log_intrusion() and creates static utility methods _create_unique_log_name() and _get_log_file_path()

--- webapp/ids/live_ids.py
+++ webapp/ids/live_ids.py
@@ -45,7 +45,35 @@ class LiveIds(object):
 	def _print_and_log_intrusion(self, log_entry, classification):
 		""" Print a warning and log the given intrusion in a new file. """
 
+		time_now = time.time()
+
+		log_file_path = LiveIds._get_log_file_path(LiveIds._create_unique_log_name())
+		with open(log_file_path, mode="w") as log_file:
+			log_file.write("Intrusion detected at ")
+		print("INTRUSION DETECTED. See log file: {}")
+
 		raise NotImplementedError()
+
+
+	@staticmethod
+	def _create_unique_log_name():
+		""" Create a unique log name based on a UUID. """
+
+		# pylint: disable-msg=C0111; (Missing method docstring)
+		def build_log_name():
+			return LiveIds.LOG_FILE_PREFIX + uuid.uuid4().__str__
+
+		name = build_log_name()
+		while os.path.lexists(LiveIds._get_log_file_path(name)):
+			name = build_log_name()
+
+		return name
+
+
+	@staticmethod
+	def _get_log_file_path(log_file_name):
+		""" Build a path from the given file name and the log folder. """
+		return LiveIds.LOG_DIR + log_file_name
 
 
 # pylint: disable-msg=R0903; (Too few public methods)

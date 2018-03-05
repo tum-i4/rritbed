d20d4f8a7d8f16f453fa987936bf6de1a2f74dc3
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-05 01:50:22 +0100

    Live Ids: Adds classification enum, ctor and stub process() method

--- webapp/ids/live_ids.py
+++ webapp/ids/live_ids.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python
 """ Live IDS """
 
+import os
+import time
+import uuid
+from enum import Enum
+
 class LiveIds(object):
 	""" Live intrusion detection """
 
-	pass
+	LOG_DIR = "log"
+	LOG_FILE_PREFIX = "log"
+
+	def __init__(self):
+		""" Ctor """
+
+		object.__init__(self)
+
+
+	def process(self, log_entry):
+		""" Process the given entry. Outputs a warning when the detection was successful. """
+
+		raise NotImplementedError()
+
+
+# pylint: disable-msg=R0903; (Too few public methods)
+class IdsClassification(Enum):
+	""" IDS classification results """
+
+	normal = 0
+	intrusion = 1

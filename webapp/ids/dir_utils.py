2f94294cf419e6b43e51495fb132bd3b31c1e2d3
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-09 13:49:49 +0100

    Live Ids, LogDir: Moves log dir / path functionality from Live Ids to new LogDir class

--- webapp/ids/dir_utils.py
+++ webapp/ids/dir_utils.py
@@ -1,8 +1,60 @@
 #!/usr/bin/env python
 """ Dir utils """
 
+import os
+import time
+import uuid
+
+
+IDS_DIR = "ids"
+
 
 class LogDir(object):
 	""" Log directory and path handling in the IDS. """
 
-	pass
+	LOG_DIR = "log"
+	LOG_FILE_PREFIX = "intrusion_"
+	LOG_FILE_SUFFIX = ".log"
+
+
+	@staticmethod
+	def create_unique_log_name():
+		""" Create a unique log name based on a UUID. """
+		return LogDir.create_unique_name(lambda: (
+			LogDir.LOG_FILE_PREFIX + uuid.uuid4().__str__() + LogDir.LOG_FILE_SUFFIX))
+
+
+	@staticmethod
+	def create_unique_backup_folder_name():
+		""" Create a unique name for a backup folder based on the current time. """
+		return LogDir.create_unique_name(lambda: (
+			"logs_until_" + time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())))
+
+
+	@staticmethod
+	def create_unique_name(name_creator):
+		""" Generic name creator method ensuring uniqueness. """
+
+		name = name_creator()
+		while os.path.lexists(LogDir.get_log_path_for(name)):
+			name = name_creator()
+
+		return name
+
+
+	@staticmethod
+	def get_log_dir():
+		""" Return the log directory. """
+
+		target_dir = LogDir.LOG_DIR
+
+		if os.path.basename(os.getcwd()) != IDS_DIR:
+			target_dir = os.path.join(IDS_DIR, target_dir)
+
+		return target_dir
+
+
+	@staticmethod
+	def get_log_path_for(name):
+		""" Build a path from the given file name and the log directory. """
+		return os.path.join(LogDir.get_log_dir(), name)
--- webapp/ids/live_ids.py
+++ webapp/ids/live_ids.py
@@ -3,17 +3,14 @@
 
 import os
 import time
-import uuid
 
 from intrusion_classifier import IntrusionClassifier
 from ids_classification import IdsResult, Classification
+from dir_utils import LogDir
+
 
 class LiveIds(object):
 	""" Live intrusion detection """
-
-	LOG_DIR = "log"
-	LOG_FILE_PREFIX = "intrusion_"
-	LOG_FILE_SUFFIX = ".log"
 
 	def __init__(self, verbose):
 		""" Ctor """
@@ -42,23 +39,23 @@ class LiveIds(object):
 
 		message = "Intrusion logs: "
 
-		if not os.path.lexists(LiveIds._get_log_dir()):
+		if not os.path.lexists(LogDir.get_log_dir()):
 			return message + "Log folder doesn't exist"
 
-		all_files = os.listdir(LiveIds._get_log_dir())
+		all_files = os.listdir(LogDir.get_log_dir())
 
 		if not all_files:
 			return message + "Log folder is empty"
 
 		# Create folder
-		folder_name = LiveIds._create_unique_backup_folder_name()
-		folder_path = LiveIds._get_log_path_for(folder_name)
+		folder_name = LogDir.create_unique_backup_folder_name()
+		folder_path = LogDir.get_log_path_for(folder_name)
 		os.mkdir(folder_path)
 
 		# Move files
 		file_count = 0
 		for file_name in all_files:
-			file_path = LiveIds._get_log_path_for(file_name)
+			file_path = LogDir.get_log_path_for(file_name)
 			if os.path.isfile(file_path) and file_path.endswith(".log"):
 				file_count += 1
 				os.rename(file_path, os.path.join(folder_path, file_name))
@@ -77,11 +74,11 @@ class LiveIds(object):
 
 		time_str = time.strftime("%A %B %d %Y - %H:%M:%S", time.localtime())
 
-		log_folder = LiveIds._get_log_dir()
+		log_folder = LogDir.get_log_dir()
 		if not os.path.lexists(log_folder):
 			os.mkdir(log_folder)
 
-		log_file_path = os.path.join(log_folder, LiveIds._create_unique_log_name())
+		log_file_path = os.path.join(log_folder, LogDir.create_unique_log_name())
 		with open(log_file_path, mode="w") as log_file:
 			LiveIds._write_line(log_file, "Intrusion detected | {}".format(time_str))
 			LiveIds._write_line(log_file, "")
@@ -92,50 +89,6 @@ class LiveIds(object):
 			LiveIds._write_line(log_file, log_entry.get_log_string())
 
 		return log_file_path
-
-
-	@staticmethod
-	def _create_unique_log_name():
-		""" Create a unique log name based on a UUID. """
-		return LiveIds._create_unique_name(lambda: (
-			LiveIds.LOG_FILE_PREFIX + uuid.uuid4().__str__() + LiveIds.LOG_FILE_SUFFIX))
-
-
-	@staticmethod
-	def _create_unique_backup_folder_name():
-		""" Create a unique name for a backup folder based on the current time. """
-		return LiveIds._create_unique_name(lambda: (
-			"logs_until_" + time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())))
-
-
-	@staticmethod
-	def _create_unique_name(name_creator):
-		""" Generic name creator method ensuring uniqueness. """
-
-		name = name_creator()
-		while os.path.lexists(LiveIds._get_log_path_for(name)):
-			name = name_creator()
-
-		return name
-
-
-	@staticmethod
-	def _get_log_dir():
-		""" Return the log directory. """
-
-		target_dir = LiveIds.LOG_DIR
-
-		ids_dir = "ids"
-		if os.path.basename(os.getcwd()) != ids_dir:
-			target_dir = os.path.join(ids_dir, target_dir)
-
-		return target_dir
-
-
-	@staticmethod
-	def _get_log_path_for(name):
-		""" Build a path from the given file name and the log directory. """
-		return os.path.join(LiveIds._get_log_dir(), name)
 
 
 	@staticmethod

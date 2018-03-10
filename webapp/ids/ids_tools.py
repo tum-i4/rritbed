6ddc7f817e4300e67f7a85045cf0e1968fd8f4d7
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-10 19:25:29 +0100

    ids_tools: Adds generate_log_entries() function for quick tests of the classifier

--- webapp/ids/ids_tools.py
+++ webapp/ids/ids_tools.py
@@ -3,6 +3,9 @@
 
 import md5
 
+import ids_data
+from log_entry import LogEntry
+
 
 def enumerate_to_dict(sequence, verify_hash):
 	""" Enumerate the given sequence and save the items in a dict as item : index pairs. """
@@ -39,3 +42,44 @@ def verify_md5(obj, md5_hex_digest):
 def get_md5_hex(obj):
 	""" Create the md5 hex hash of the given object's string representation. """
 	return md5.new(str(obj)).hexdigest()
+
+
+def generate_log_entries(number):
+	""" Generate <number> LogEntry objects. """
+	import random
+
+	result = []
+	vins = [chr(random.choice(range(65, 91))) + str(x)
+		for x in random.sample(range(100000, 900000), int(number))]
+	colr_gen = lambda: random.randint(0, 255)
+	tsp_gen = lambda: random.randint(0, 499)
+	log_msg_gens = [
+		(ids_data.GENERATORS, lambda: str(float(random.randint(-3, 2)))),
+		(ids_data.COLOURS, lambda: "{},{},{}".format(colr_gen(), colr_gen(), colr_gen())),
+		(ids_data.POSE_CC, lambda: random.choice(["DE", "AT", "CH", "FR"])),
+		(ids_data.POSE_POI,
+			(lambda: random.choice(ids_data.POI_TYPES) + "," + random.choice(ids_data.POI_RESULTS))),
+		(ids_data.POSE_TSP,
+			lambda: "{},{},{},{}".format(tsp_gen(), tsp_gen(), tsp_gen(), tsp_gen()))
+	]
+
+	for i in range(0, int(number)):
+		vin = vins[i]
+		app_id = random.choice(ids_data.APP_IDS)
+		level = random.choice(ids_data.LEVELS)
+		gps_position = "{},{}".format(tsp_gen(), tsp_gen())
+
+		log_message = None
+		for keys, gen in log_msg_gens:
+			if app_id in keys:
+				log_message = gen()
+		if not log_message:
+			raise ValueError("You suck!")
+
+		intrusion = random.choice(ids_data.LABELS)
+
+		result.append(LogEntry(
+			vin=vin, app_id=app_id, level=level, gps_position=gps_position,
+			log_message=log_message, intrusion=intrusion))
+
+	return result

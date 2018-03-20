4df75338b31166b1aefc16918dadaa431afb75f1
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-20 18:22:03 +0100

    Ids Conv: Simplifies log_entry_to_class()
    Former-commit-id: 7d57213fb76bd95ac3f994a610247beb5201349b

--- webapp/ids/ids_converter.py
+++ webapp/ids/ids_converter.py
@@ -82,16 +82,8 @@ class IdsConverter(object):
 		if not log_entry.intrusion:
 			raise ValueError("Given LogEntry does not have a set intrusion to convert.")
 
-		legal_labels = ids_data.get_legal_labels()
-		if len(legal_labels) != 1:
-			raise NotImplementedError("Did not expect more than one legal label.")
-
-		legal_mapping = self._label_int_mapping[legal_labels[0]]
-		if legal_mapping != 0:
-			raise ValueError("Expected a mapping to 0 for the legal label.")
-
-		if log_entry.intrusion in legal_labels:
-			return legal_mapping
+		if log_entry.intrusion in ids_data.get_legal_labels():
+			return 0
 		elif log_entry.intrusion in ids_data.get_intrusion_labels():
 			return 1
 		else:

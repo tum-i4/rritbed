408f9be2caeb3f36ef7ee6fd2cdd54261159d9f3
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-20 18:20:32 +0100

    Ids Conv: Removes multi-class support from log_entry_to_class()
    Former-commit-id: 6f784d389b0b536f03e511e884f543c1ac5b8e2b

--- webapp/ids/ids_converter.py
+++ webapp/ids/ids_converter.py
@@ -76,18 +76,16 @@ class IdsConverter(object):
 		return result
 
 
-	def log_entry_to_class(self, log_entry, multi_class):
+	def log_entry_to_class(self, log_entry):
 		""" Map the given LogEntry object to a class to predict. """
 
 		if not log_entry.intrusion:
 			raise ValueError("Given LogEntry does not have a set intrusion to convert.")
 
-		if multi_class:
-			return self._label_int_mapping[log_entry.intrusion]
-
 		legal_labels = ids_data.get_legal_labels()
 		if len(legal_labels) != 1:
 			raise NotImplementedError("Did not expect more than one legal label.")
+
 		legal_mapping = self._label_int_mapping[legal_labels[0]]
 		if legal_mapping != 0:
 			raise ValueError("Expected a mapping to 0 for the legal label.")
@@ -96,6 +94,8 @@ class IdsConverter(object):
 			return legal_mapping
 		elif log_entry.intrusion in ids_data.get_intrusion_labels():
 			return 1
+		else:
+			raise ValueError("Unexpected intrusion type encountered: {}".format(log_entry.intrusion))
 
 
 	@staticmethod

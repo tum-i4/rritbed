e3078343dbb3cd2eb621ca3ced7f2ce8883e3eeb
vauhochzett <vauhochzett@users.noreply.github.com>
2018-03-20 22:53:02 +0100

    ids_tools.Printer: Adds support for newline characters in the printed message
    Former-commit-id: e85f4ad973883a6acc00959fea19eb45e8b1ef67

--- webapp/ids/ids_tools.py
+++ webapp/ids/ids_tools.py
@@ -100,18 +100,25 @@ class Printer(object):
 		self.name = name
 		self.last_ended_in_newline = True
 
+
 	def prt(self, message, preface=True, newline=True):
-		""" Print if not squelching. """
+		""" Print if not squelching. Handles newline characters in the message. """
+
+		for line in message.split("\n"):
+			self._prt_line(line, preface, newline)
+
+
+	def _prt_line(self, line, preface=True, newline=True):
+		""" Print single line. """
 
 		if self.squelch:
 			return
 
 		if preface and self.name and self.last_ended_in_newline:
-			message = "[{}] {}".format(self.name, message)
-
+			line = "[{}] {}".format(self.name, line)
 		self.last_ended_in_newline = newline
 
-		print(message, end="\n" if newline else "")
+		print(line, end="\n" if newline else "")
 		sys.stdout.flush()
 
 

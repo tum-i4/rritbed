e1364d04ea454f5ce2cdf703426481758a6b8cc5
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-05 14:29:39 +0200

    Intr Clas: Moves log entry streaming to separate method
    Former-commit-id: 97de81f2ef2f76f2edfa729826d75bf8556f4a00

--- webapp/ids/intrusion_classifier.py
+++ webapp/ids/intrusion_classifier.py
@@ -129,7 +129,21 @@ class IntrusionClassifier(object):
 		printer.prt("Streaming from file up to a maximum of {} entries.".format(limit))
 		printer.prt("Loading and converting entries...")
 
-		total_entry_count = 0
+		found_app_ids, converted_entries = self._stream_convert_entries(
+			log_entry_generator, limit, printer)
+
+		self._train_entries(converted_entries, printer)
+
+		printer.prt("Finished training classifiers for {}/{} app ids."
+			.format(len(found_app_ids), len(self._converter.app_ids)))
+
+
+	def _stream_convert_entries(self, log_entry_generator, limit, printer):
+		"""
+		Read up to <limit> entries from disk and convert them to (app_id, vector, class) tuples.
+		returns: found_app_ids (set) and converted_entries (tuple list)
+		"""
+
 		found_app_ids = set()
 		# Save tuple (app_id, vector, class) for lower memory usage
 		converted_entries = []
@@ -137,15 +151,11 @@ class IntrusionClassifier(object):
 			if len(converted_entries) == limit:
 				break
 
-			total_entry_count += 1
 			converted_entry = self._log_entry_to_prepared_tuple(log_entry)
 			found_app_ids.add(converted_entry[0])
 			converted_entries.append(converted_entry)
 
-		self._train_entries(converted_entries, printer)
-
-		printer.prt("Finished training classifiers for {}/{} app ids."
-			.format(len(found_app_ids), len(self._converter.app_ids)))
+		return (found_app_ids, converted_entries)
 
 
 	def _train_entries(self, converted_entries, printer):

c81c69fbc3c8dc65bc0cb1e31e418123e0b0159b
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-08 01:52:07 +0200

    a: Removes splitting and scoring from a(), adds b() (raising)
    Former-commit-id: b1e322140afaa19bb0389bcff5e581c560e48195

--- webapp/a.py
+++ webapp/a.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# pylint: disable-msg=C0103; (Invalid name)
+
 import argparse
 
 from log_entry import LogEntry
@@ -10,30 +12,32 @@ from ids.dir_utils import Dir
 from ids.ids_converter import IdsConverter
 
 
+### Workers ###
+
+
 def a(file_path):
+	""" Read entries, convert them, split them per app_id and call b() for each app. """
 
 	printer = util.prtr.Printer(name="a")
-	squelcher = util.prtr.Printer(squelch=True)
-	converter = IdsConverter()
-
 	printer.prt("Reading file and converting...")
 
 	# converted_entries: [(app_id, vector, class)]
 	converted_entries = read_convert(file_path)
 
-	scores_acc = _empty_app_id_dict()
-	scores_prec = _empty_app_id_dict()
-	scores_rec = _empty_app_id_dict()
+	entries_per_app = _empty_app_id_tuple_dict()
+	for app_id, vector, its_class in converted_entries:
+		entries_per_app[app_id][0].append(vector)
+		entries_per_app[app_id][1].append(its_class)
 
-	printer.prt("Splitting... ")
-	train_entries, test_entries = ids_tools.converted_entries_to_train_test(converted_entries)
-	train_dict = converter.prepared_tuples_to_train_dict(train_entries, squelcher)
-	test_dict = converter.prepared_tuples_to_train_dict(test_entries, squelcher)
+	for app_id, (X, y) in entries_per_app.items():
+		b(X, y)
 
-	for app_id in converter.app_ids:
-		pass
 
+def b(X, y):
 	raise NotImplementedError()
+
+
+### Helpers ###
 
 
 def read_convert(file_path):

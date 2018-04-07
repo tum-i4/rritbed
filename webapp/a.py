139b38102564845bc47a662c637b71ec0a33282f
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-08 01:24:23 +0200

    a: Adds read_convert()
    Former-commit-id: c18fe042765f66d73045e3ac79981024fcba8fcd

--- webapp/a.py
+++ webapp/a.py
@@ -7,6 +7,20 @@ def a(file_path):
 	raise NotImplementedError()
 
 
+def read_convert(file_path):
+	""" Read log entries from the given file and convert the result. """
+
+	converter = IdsConverter()
+	converted_entries = []
+
+	for line in Dir.yield_lines(file_path, 5000000):
+		log_entry = LogEntry.from_log_string(line)
+		converted_entry = converter.log_entry_to_prepared_tuple(log_entry, binary=True)
+		converted_entries.append(converted_entry)
+
+	return converted_entries
+
+
 if __name__ == "__main__":
 	try:
 		PARSER = argparse.ArgumentParser()

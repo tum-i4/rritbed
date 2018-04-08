417214eead3d7c9cacc9448cc2db51a237c83052
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-08 23:10:56 +0200

    a: Rewrites read_convert() to directly utilise the new log_entries_to_*_dict() method in IdsConverter
    Former-commit-id: 1609555c0d314afeddecfb878ad2e30bb411e70b

--- webapp/a.py
+++ webapp/a.py
@@ -27,11 +27,7 @@ def a(file_path):
 	printer.prt("Reading file and converting...")
 
 	# ids_entries: { app_id, vector, my_class }
-	ids_entries = read_convert(file_path)
-
-	ids_entries_per_app = ids_tools.empty_app_id_to_list_dict()
-	for ids_entry in ids_entries:
-		ids_entries_per_app[ids_entry.app_id].append(ids_entry)
+	ids_entries_per_app = read_convert(file_path)
 
 	for app_id, ids_entries in ids_entries_per_app.items():
 		b(app_id, ids_entries)
@@ -106,14 +102,14 @@ def read_convert(file_path):
 	""" Read log entries from the given file and convert the result. """
 
 	converter = IdsConverter()
-	ids_entries = []
+	log_entries = []
 
 	for line in Dir.yield_lines(file_path, 5000000):
 		log_entry = LogEntry.from_log_string(line)
-		ids_entry = converter.log_entry_to_ids_entry(log_entry, binary=True)
-		ids_entries.append(ids_entry)
+		log_entries.append(log_entry)
 
-	return ids_entries
+	ids_entries_dict = converter.log_entries_to_ids_entries_dict(log_entries)
+	return ids_entries_dict
 
 
 def unravel_ids_entries(ids_entries, expected_app_id):

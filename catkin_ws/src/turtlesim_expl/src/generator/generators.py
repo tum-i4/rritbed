80518be4d2601225b7da22f01b67619ecd8ed70a
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-10 23:42:51 +0200

    Gens: Updates with more precise range values; adds mean
    Former-commit-id: f07493f1507c1a0eae53fb8fd9aad309d1d3735d

--- catkin_ws/src/turtlesim_expl/src/generator/generators.py
+++ catkin_ws/src/turtlesim_expl/src/generator/generators.py
@@ -3,7 +3,6 @@
 
 import argparse
 import json
-import math
 import os
 
 import numpy as np
@@ -27,47 +26,48 @@ EASY_STR = "easy"
 MED_STR = "med"
 HARD_STR = "hard"
 
+# Rounded to five decimal places
 
 # pylint: disable-msg=E1101
 GENERATORS = {
 	# Gaussian, Gumbel, Laplace: loc and scale arbitrary
 	GAUSSIAN_STR : DistributionGenerator("normal", GAUSSIAN_STR,
 		[AC(0.0), AC(1.0)],
-		expected_values=[-3.5, 3.5]),
+		expected_range=[-3.09023, 3.09023], mean=0.0),
 	GUMBEL_STR : DistributionGenerator("gumbel", GUMBEL_STR,
 		[AC(0.0), AC(1.0)],
-		expected_values=[-7, 2.3]),
+		expected_range=[-6.90726, 1.93264], mean=-0.57722),
 	LAPLACE_STR : DistributionGenerator("laplace", LAPLACE_STR,
 		[AC(0.0), AC(1.0)],
-		expected_values=[-6.3, 6.3]),
+		expected_range=[-6.21461, 6.21461], mean=0.0),
 	# Logistic: loc arbitrary, scale > 0
 	LOGISTIC_STR : DistributionGenerator("logistic", LOGISTIC_STR,
 		[AC(0.0), AC(1.0, min_value=0.1)],
-		expected_values=[-7, 7]),
+		expected_range=[-6.90675, 6.90675], mean=0.0),
 	# Pareto: a(lpha) > 0
 	PARETO_STR : DistributionGenerator("pareto", PARETO_STR,
-		[AC(1.0, min_value=0.1)],
-		expected_values=[0, 30.7]),
+		[AC(2.0, min_value=0.1)],
+		expected_range=[0.00050, 30.62278], mean=1.0),
 	# Rayleigh: scale > 0
 	RAYLEIGH_STR : DistributionGenerator("rayleigh", RAYLEIGH_STR,
 		[AC(1.0, min_value=0.1)],
-		expected_values=[0, 4.1]),
+		expected_range=[0.04473, 3.71692], mean=1.25331),
 	# Uniform: low < high (not a binding constraint)
 	UNIFORM_STR : DistributionGenerator("uniform", UNIFORM_STR,
 		[AC(0.0), AC(1.0)],
-		expected_values=[0, 1]),
+		expected_range=[0, 1], mean=0.5),
 	# Von Mises: mu arbitrary, kappa >= 0
 	VON_MISES_STR : DistributionGenerator("vonmises", VON_MISES_STR,
 		[AC(0.0), AC(1.0, min_value=0)],
-		expected_values=[-math.pi, math.pi]),
+		expected_range=[-3.11997, 3.11997], mean=0.00000),
 	# Wald: mean > 0, scale > 0
 	WALD_STR : DistributionGenerator("wald", WALD_STR,
 		[AC(1.0, min_value=0.1), AC(1.0, min_value=0.1)],
-		expected_values=[0, 7.8]),
+		expected_range=[0.07922, 8.35486], mean=1.0),
 	# Weibull: a > 0
 	WEIBULL_STR : DistributionGenerator("weibull", WEIBULL_STR,
 		[AC(5.0, min_value=0.1)],
-		expected_values=[0.1, 1.6])
+		expected_range=[0.25121, 1.47186], mean=0.91817)
 }
 
 

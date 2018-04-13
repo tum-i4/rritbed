1225cad9cd5a83ecd1f4bf5bdd6b963f2167a188
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-14 00:10:46 +0200

    IDSE DAO: Adds basic file existance checks
    Former-commit-id: 1fe617f216841e302f63810460d6d2fafac232fd

--- webapp/ids_entry_dao.py
+++ webapp/ids_entry_dao.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 """ Convenient access to IDS entries, stored in various forms. """
 
+import os
+
+
 def get_entries(file_path, limit=None):
+	""" Get a generator of IdsEntry objects from the given file.
+	First retrieval might be slower than others. """
+
+	if not os.path.lexists(file_path):
+		raise IOError("File not found: %s" % file_path)
+
 	raise NotImplementedError()
 
 
 def save_entries(file_path, entries):
+	""" Store the entries as a IDSE file. """
+
+	if os.path.lexists(file_path):
+		raise IOError("Output file exists already: %s" % file_path)
+
 	raise NotImplementedError()

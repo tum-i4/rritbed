d4ef6ff005ca1bc2ca3aba4d7491618747eb5b17
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-13 23:56:22 +0200

    Adds new ids_entry_dao.py for loading and saving ids entries
    Former-commit-id: baba2c7d68a7b74830cefea89b66252e2704f317

---
+++ webapp/ids_entry_dao.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+""" Convenient access to IDS entries, stored in various forms. """
+
+def get_entries(file_path, limit=None):
+	raise NotImplementedError()
+
+
+def save_entries(file_path, entries):
+	raise NotImplementedError()

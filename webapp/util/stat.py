b9d7122b8e8c2b297c2e7484253070de4911e05b
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-17 18:31:40 +0200

    util.stat: Adds index_of_dispersion()
    Former-commit-id: dca906f8678f166dbf58c8d0ba2417f13faa2adf

--- webapp/util/stat.py
+++ webapp/util/stat.py
@@ -7,3 +7,9 @@ import statistics as stats
 def avg(sequence):
 	""" Short-hand for statistics.mean(). """
 	return stats.mean(sequence)
+
+
+def index_of_dispersion(sequence):
+	""" Calculate the variance-to-mean ratio of a sequence. """
+	float_seq = [float(x) for x in sequence]
+	return stats.pvariance(float_seq) / stats.mean(float_seq)

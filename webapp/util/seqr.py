fe16b10045974f8c68415bfd89377c3e13eb63e6
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-17 18:32:16 +0200

    util.seqr: Adds yield_items_in_key_order() short-hand for consistent result output
    Former-commit-id: 9286898f6e445f5358b3161256ac7b791f61da09

--- webapp/util/seqr.py
+++ webapp/util/seqr.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 """ Sequence utils """
+
+
+def yield_items_in_key_order(any_dict):
+	""" Replacement for items() that sorts keys before returning. """
+
+	sorted_keys = sorted(any_dict.keys())
+	for key in sorted_keys:
+		yield (key, any_dict[key])

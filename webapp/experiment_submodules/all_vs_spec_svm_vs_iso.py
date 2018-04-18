4ab5734323895232c33fadefb24c71486dbe868b
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-18 02:55:09 +0200

    Adds experiment_submodules and in it all_vs_spec_svm_vs_iso.py with raising run()
    Former-commit-id: 0d427a45468421e1db34e13dbac1d214c9f5ccc8

---
+++ webapp/experiment_submodules/all_vs_spec_svm_vs_iso.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Experiment:
+Classifier trained on all vs specialised classifiers,
+OneClassSVM vs IsolationForest.
+"""
+
+def run(experiment):
+	raise NotImplementedError()

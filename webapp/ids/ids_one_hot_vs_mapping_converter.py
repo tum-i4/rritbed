b7bdd97d763f4f351ae67fd12a69895cabf0de68
vauhochzett <vauhochzett@users.noreply.github.com>
2018-04-20 14:11:05 +0200

    OneHotVsMappingConverter: Fixes super init
    Former-commit-id: 64dbc3b1a811d323110502ed8285e2291ae700ae

--- webapp/ids/ids_one_hot_vs_mapping_converter.py
+++ webapp/ids/ids_one_hot_vs_mapping_converter.py
@@ -11,7 +11,7 @@ import ids_tools
 class OneHotVsMappingConverter(IdsConverter):
 
 	def __init__(self):
-		super(OneHotVsMappingConverter, self).__init__(self)
+		super(OneHotVsMappingConverter, self).__init__()
 
 
 	@staticmethod
